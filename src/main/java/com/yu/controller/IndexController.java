e97159c2a43dc24edebb1e8e212d142e869f53f3
YZLBei <yuzhenlongbei@outlook.com>
2022-07-03 19:46:34 +0800

    首页

--- src/main/java/com/yu/controller/IndexController.java
+++ src/main/java/com/yu/controller/IndexController.java
@@ -1,12 +1,19 @@
 package com.yu.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * @Auther: YuZhenLong
  * @Date: 2022/7/3 17:11
- * @Description:
+ * @Description:不建议这么使用，应该用视图控制器映射
  */
 @Controller
 public class IndexController {
+    
+//    //不建议这么使用，应该用视图控制器映射
+//    @RequestMapping({"/","/index.html"})
+//    public String index(){
+//        return "index";
+//    }
 }
