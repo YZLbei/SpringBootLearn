cf2bbdb1591b9a2a38e9932fa70917b0c284cafa
YZLBei <yuzhenlongbei@outlook.com>
2022-07-05 20:13:55 +0800

    实现登录功能

---
+++ src/main/java/com/yu/config/LoginHandlerInterceptor.java
@@ -0,0 +1,11 @@
+package com.yu.config;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+
+/**
+ * @Auther: YuZhenLong
+ * @Date: 2022/7/5 20:13
+ * @Description:
+ */
+public class LoginHandlerInterceptor implements HandlerInterceptor {
+}
--- src/main/java/com/yu/controller/LoginController.java
+++ src/main/java/com/yu/controller/LoginController.java
@@ -1,9 +1,35 @@
 package com.yu.controller;
 
+
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.thymeleaf.util.StringUtils;
+
 /**
  * @Auther: YuZhenLong
  * @Date: 2022/7/3 20:59
  * @Description:
  */
+@Controller
 public class LoginController {
+    @RequestMapping("/user/login")
+    public String login(
+            @RequestParam("username") String username,
+            @RequestParam("password") String password,
+            Model model){
+        if (!StringUtils.isEmpty(username)&&password.equals("123456")){
+            return "dashboard";
+        } 
+        else {
+            //告诉用户登陆失败
+            model.addAttribute("msg","用户名或者密码错误");
+            return "index";
+        }
+    }
 }
